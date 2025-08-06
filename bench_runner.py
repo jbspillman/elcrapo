0c71ab70048631f859cf86d4df3eaaef90fde84a
spillman <jbspillman@gmail.com>
2025-08-06 04:23:46 -0400

    latest updates

--- bench_runner.py
+++ bench_runner.py
@@ -1,9 +1,8 @@
+from bench_charter import create_reports
 from datetime import datetime
 import timeit
 import os
-import json
 import time
-from doctest import testsource
 
 ''' the storage targets for tests.
     > the targets are local to the server running the script or remote nodes.
@@ -48,7 +47,34 @@ def show_the_text():
 
     time.sleep(3)
     print()
-    
+
+def the_cleaner(list_of_things):
+
+    for folder_or_file_to_delete in list_of_things:
+        if os.path.exists(folder_or_file_to_delete):
+            if os.path.isdir(folder_or_file_to_delete):
+                try:
+                    os.rmdir(folder_or_file_to_delete)
+                except OSError as err_msg:
+                    if 'Errno 39' in str(err_msg):
+                        for f_name in os.listdir(folder_or_file_to_delete):
+                            fp = os.path.join(folder_or_file_to_delete, f_name)
+                            if os.path.exists(fp) and f_name.endswith(".BIN"):
+                                print('removing test file in folder:', fp)
+                                try:
+                                    os.remove(fp)
+                                except FileNotFoundError:
+                                    pass
+            else:
+
+                if os.path.exists(folder_or_file_to_delete) and folder_or_file_to_delete.endswith(".BIN"):
+                    print('removing test file in folder:', folder_or_file_to_delete)
+                    try:
+                        os.remove(folder_or_file_to_delete)
+                    except FileNotFoundError:
+                        pass
+    time.sleep(5)
+
 
 def run_bench_marks():
 
@@ -75,20 +101,22 @@ def run_bench_marks():
             "test_block": "4K"
         },
     ]
-    threads_and_sizes_list = ["8;1", "4;2", "2;4", "1;8"] # "Threads;GB"
+    test_target_types = ["directory_based"]  # "directory_based", "file_based"
+    threads_and_sizes_list = ["1;20", "2;10", "4;5", "8;2.5", "16;1.25"] # "Threads;GB"
     iodepth = 1
 
-    extras_00 = "--cpu --lat --log 0 --timelimit 900 --no0usecerr --dirsharing --files 1 --dirs 1 --mkdirs --deldirs --delfiles"
-    extras_01 = "--live1 --livecsvex --liveint 1000" # --dryrun --direct 
+    directory_extras = "--cpu --lat --log 1 --timelimit 900 --no0usecerr --dirsharing --files 1 --dirs 1 --mkdirs --deldirs --delfiles"
+    file_extras = "--cpu --lat --log 1 --timelimit 900 --no0usecerr"
+    extra_options = "--live1 --livecsvex --liveint 1000" # --dryrun --direct
     full_cmd = ""
-    cleanup_files_list = []
+    cleanup_list = []
     target_names = []
     all_labels = []
 
     ''' Threads and File Sizes are same length of a list. '''
     threads_sizes = len(threads_and_sizes_list)
-    types_of_jobs = 2  # RANDOM 4K and SEQUENTIAL 1M
-    total_tests = len(storage_targets) * threads_sizes * types_of_jobs
+    types_of_jobs = len(two_corners_x_write_read)  # RANDOM 4K and SEQUENTIAL 1M
+    total_tests = len(storage_targets) * threads_sizes * types_of_jobs * len(test_target_types)
     tests_per_target = round(total_tests / len(storage_targets))
     
     tests_per_target = str(tests_per_target).zfill(3)
@@ -96,8 +124,9 @@ def run_bench_marks():
     
     print('')
     print("storage_targets:".ljust(30), len(storage_targets))
-    print("threads_and_sizes:".ljust(30), threads_sizes)
+    print("test_target_types:".ljust(30), len(test_target_types))
     print("types_of_jobs:".ljust(30), types_of_jobs)
+    print("threads_and_sizes:".ljust(30), threads_sizes)
     print("tests_per_target:".ljust(30), tests_per_target)
     print("total_tests:".ljust(30), total_tests)
     time.sleep(5)
@@ -113,8 +142,10 @@ def run_bench_marks():
     os.makedirs(test_folder, exist_ok=True)
 
     ''' perform the tests on the storage targets '''
+    group = 0
     for tgt in storage_targets:
-        print('')
+        group += 1
+        group_pad = str(group).zfill(3)
         test_number = 0  # reset each storage target.
         target_name = tgt["target_name"]
         target_names.append(target_name)
@@ -123,107 +154,113 @@ def run_bench_marks():
         target_test_directory = os.path.join(target_path, 'benchmark_tests', 'data_testing', target_name)
         os.makedirs(target_test_directory, exist_ok=True)
 
-        ''' create test commands '''
-        for test_item in two_corners_x_write_read:
-            ''' get the test parameters '''
-            test_name = test_item["test_name"]
-            test_code =  test_item["test_code"]
-            test_mode = test_item["test_mode"]
-            block_size = test_item["test_block"]
-            
-            for threads_n_size in threads_and_sizes_list:
-                threads = threads_n_size.split(';')[0]
-                file_size = threads_n_size.split(';')[1]
-                
-                test_number += 1
-                test_pad = str(test_number).zfill(3)
-                test_label = f'{test_pad}_{test_name}__{file_size}G_{block_size}_{threads}T_{iodepth}D__{target_name}'.upper()
-                res_file = os.path.join(test_folder, f"{target_name}_{test_label}.txt")
-                csv_file = os.path.join(test_folder, f"{target_name}_{test_label}.csv")
-                csv_live = os.path.join(test_folder, f"{target_name}_{test_label}.live")
+        ''' Easier to ramp up the thread count per test then move on, for parsing later. '''
+        for threads_n_size in threads_and_sizes_list:
+            threads = threads_n_size.split(';')[0]
+            file_size = threads_n_size.split(';')[1]
+            for test_target_type in  test_target_types:
+                ''' create test commands '''
+                for test_item in two_corners_x_write_read:
+                    ''' get the test parameters '''
+                    test_name = test_item["test_name"]
+                    test_code =  test_item["test_code"]
+                    test_mode = test_item["test_mode"]
+                    block_size = test_item["test_block"]
+                    test_number += 1
+                    test_pad = str(test_number).zfill(3)
+                    test_label = f'{test_pad}_{test_name}_{target_name}_{file_size}G_{block_size}_{threads}T_{iodepth}D'.upper()
+                    file_label = f'{target_name}_{test_name}_{test_target_type}'.upper()
+                    res_file = os.path.join(test_folder, f"{file_label}.txt")
+                    csv_file = os.path.join(test_folder, f"{file_label}.csv")
+                    csv_live = os.path.join(test_folder, f"{file_label}.live")
 
-                ''' create random file name for test '''
-                file_stamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
-                data_file_name = f'{file_stamp}_{test_label}.BIN'.upper()
-                test_file_path = os.path.join(target_test_directory, data_file_name)
-                cleanup_files_list.append(target_test_directory)
-                cleanup_files_list.append(test_file_path)
+                    ''' create random file name for test '''
+                    file_stamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+                    data_file_name = f'{file_stamp}_{test_label}.BIN'.upper()
+                    test_file_path = os.path.join(target_test_directory, data_file_name)
+                    if test_target_type == "directory_based":
+                        data_file_name = None
+                        extras = directory_extras
+                        test_target_path = f'{target_test_directory}/'
+                        cleanup_list.append(str(target_test_directory))
+                    else:
+                        extras = file_extras
+                        test_target_path = test_file_path
+                        cleanup_list.append(str(test_target_path))
 
-                print()
-                print('test_number                  : ', test_pad, f'of {tests_per_target} Total: {total_tests}')
-                print('target_name                  : ', target_name)
-                print('test_name                    : ', test_name)
-                print('test_code                    : ', test_code)
-                print('test_label                   : ', test_label)
-                print('test_mode                    : ', test_mode)
-                print('file_size                    : ', f'{file_size}G')
-                print('threads                      : ', threads)
-                print('block_size                   : ', block_size)
-                print('iodepth                      : ', iodepth)
-                print('res_file                     : ', res_file)
-                print('csv_file                     : ', csv_file)
-                print('csv_live                     : ', csv_live)
-                print('extras_00                    : ', extras_00)
-                print('extras_01                    : ', extras_01)
-                print('target_test_directory        : ', target_test_directory)
-                print('data_file_name               : ', data_file_name)
+                    print()
+                    print()
+                    print('test_number                  : ', test_pad, f'of {tests_per_target} | Group: [{group_pad}] Total: {total_tests}')
+                    print('target_name                  : ', target_name)
+                    print('test_name                    : ', test_name)
+                    print('test_code                    : ', test_code)
+                    print('test_label                   : ', test_label)
+                    print('test_mode                    : ', test_mode)
+                    print('file_size                    : ', f'{file_size}G')
+                    print('threads                      : ', threads)
+                    print('block_size                   : ', block_size)
+                    print('iodepth                      : ', iodepth)
+                    print('file_label                   : ', file_label)
+                    print('res_file                     : ', res_file)
+                    print('csv_file                     : ', csv_file)
+                    print('csv_live                     : ', csv_live)
+                    print('extras                       : ', extras)
+                    print('extra_options                : ', extra_options)
+                    print('test_target_type             : ', test_target_type)
+                    print('test_target_path             : ', test_target_path)
+                    print('data_file_name               : ', data_file_name)
 
-                if test_label not in all_labels:
-                    all_labels.append(test_label)
-                else:
-                    print('#################################')
-                    print('#################################')
-                    print('test_label has already been done!')
-                    print('#################################')
-                    print('#################################')
-                    time.sleep(5)
+                    if test_label not in all_labels:
+                        all_labels.append(test_label)
+                    else:
+                        print('#################################')
+                        print('#################################')
+                        print('test_label has already been done!')
+                        print('#################################')
+                        print('#################################')
+                        time.sleep(5)
+                    
+                    file_size_mb = round(float(file_size) * 1024)
+                    
+                    full_cmd = (f'{elbencho_exe} '
+                                f'--label {test_label} '
+                                f'{test_mode} '
+                                f'{extras} '
+                                f'{extra_options} '
+                                f'--iodepth={iodepth} '
+                                f'--threads={threads} '
+                                f'--block={block_size} '
+                                f'--size={file_size_mb}M '
+                                f'--csvfile {csv_file} '
+                                f'--resfile {res_file} '
+                                f'--livecsv {csv_live} '
+                                f'{test_target_path} '
+                                )
+                    start_it = timeit.default_timer()
+                    os.system(full_cmd)
+                    finish_it = timeit.default_timer()
+                    test_time = finish_it - start_it
+                    print('test_time                    : ', test_time)
+                    print()
 
-                full_cmd = (f'{elbencho_exe} '
-                            f'--label {test_label} '
-                            f'{test_mode} '
-                            f'{extras_00} '
-                            f'{extras_01} '
-                            f'--iodepth={iodepth} '
-                            f'--threads={threads} '
-                            f'--block={block_size} '
-                            f'--size={file_size}G '
-                            f'--csvfile {csv_file} '
-                            f'--resfile {res_file} '
-                            f'--livecsv {csv_live} '
-                            f'{target_test_directory} '
-                            )
-                start_it = timeit.default_timer()
-                os.system(full_cmd)
-                finish_it = timeit.default_timer()
-                test_time = finish_it - start_it
-                print('test_time                    : ', test_time)
-                print()
-                # if 'dryrun' not in full_cmd:
-                    # for file_to_delete in cleanup_files_list:
-                        # if os.path.exists(file_to_delete):
-                            # print('removing test files:', file_to_delete)
-                            # try:
-                                # os.remove(file_to_delete)
-                            # except FileNotFoundError:
-                                # pass
-                    # time.sleep(5)
 
-    # if 'dryrun' in full_cmd:
-        # os.rmdir(test_folder)
-        # for file_to_delete in cleanup_files_list:
-            # if os.path.exists(file_to_delete):
-                # print('removing test files:', file_to_delete)
-                # try:
-                    # os.remove(file_to_delete)
-                # except FileNotFoundError:
-                    # pass
+    cleanup_list = list(set(cleanup_list))
+    the_cleaner(cleanup_list)
+    time.sleep(3)
 
-    return target_names
+    if 'dryrun' in full_cmd:
+        os.rmdir(test_folder)
+        test_folder = None
+    return test_folder
 
 
 def main():
-    run_bench_marks()
-
+    results_folder = run_bench_marks()
+    
+    print(f'use: {results_folder}')
+    time.sleep(3)
+    
+    create_reports(results_folder)
 
 
 if __name__ == "__main__":
